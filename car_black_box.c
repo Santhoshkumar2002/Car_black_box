1df9e029d38c67e6aa1a2447c6589b09cdffafbe
Santhoshkumar2002 <santhoshkumar2002r@gmail.com>
2023-12-15 13:30:25 +0530

    update read_password function

--- car_black_box.c
+++ car_black_box.c
@@ -9,11 +9,13 @@
 #include <xc.h>
 #include "car_black_box.h"
 
+extern unsigned int sec;
 unsigned char *gear_data[8] = {"ON", "GN", "G1", "G2", "G3", "G4", "GR", "C "};
 unsigned short gear_index = 0, speed;
 
 void display_dashboard(unsigned char key) {
     clcd_print("  TIME    E  SP ", LINE1(0));
+    clcd_print("                ", LINE2(0));
     display_time();
     gear_monitor(key);
     speed = (read_adc(4) / 10.23);
@@ -47,7 +49,8 @@ void display_speed(unsigned short speed) {
     clcd_putch((speed % 10) + 48, LINE2(14));
 }
 
-void display_password() {
+void read_password() {
+    sec = 0;
     unsigned char pass_key, attempt = '3';
     unsigned short index = 0, wait = 0, ind, flag = 0;
     unsigned char *original_password = "0011", enter_password[5];
@@ -58,11 +61,20 @@ void display_password() {
         if (flag == 0) {
             pass_key = read_switches(EDGE);
             if (pass_key == 11) {
-                enter_password[index++] = '1';
+                enter_password[index] = '1';
+                sec = 0;
+                index++;
             } else if (pass_key == 12) {
-                enter_password[index++] = '0';
+                enter_password[index] = '0';
+                sec = 0;
+                index++;
             }
 
+            if(sec == 5)
+            {
+                return;
+            }
+            
             for (ind = 0; ind < index; ind++) {
                 clcd_putch('*', LINE2(ind + 3));
             }
@@ -95,12 +107,6 @@ void display_password() {
                     }
                     ind_compare++;
                 }
-                if (attempt == '0') {
-                    clcd_print("  Attempt Over  ", LINE1(0));
-                    clcd_print("                ", LINE2(0));
-                    __delay_ms(2000);
-                    return;
-                }
                 if (count == 0)
                 {
                     flag = 1;
@@ -108,6 +114,20 @@ void display_password() {
                     clcd_print("Correct Password", LINE1(0));
                     clcd_print("   Menu Page    ", LINE2(0));
                 }
+                else if(attempt == '0') {
+                    clcd_print("  Attempt Over  ", LINE1(0));
+                    clcd_print("                ", LINE2(0));
+                    sec = 0;
+                    while(sec != 60)
+                    {
+                    clcd_print("   Wait For     ", LINE1(0));
+                    clcd_putch((59-sec)/10+48, LINE2(0));
+                    clcd_putch(((59-sec)%10)+48, LINE2(1));
+                    clcd_print("Seconds remain",LINE2(2));
+                    }
+                    return;
+                }
+                
             }
         } else if (flag == 1) {
             __delay_ms(1000);
